4429e53732e987fdb800a18696d26b2a7e5a5b5b
Serhii T <tocarchuk@gmail.com>
2021-11-03 10:57:13 +0200

    Add image to Home page

--- src/main-home.js
+++ src/main-home.js
@@ -1,17 +1,27 @@
 import { containerDiv } from "./index";
+import Restaurant from './restaurant.jpg';
 
 function render() {
   const content = document.createElement('div');
   const welcome = document.createElement('h3');
   const restaurantName = document.createElement('h1');
+  // add restaurant image on Home page
+  const image = new Image();
+  image.src = Restaurant;
+
+  const menuButton = document.createElement('button');
 
   content.classList.add('content');
   welcome.innerText = 'bienvenue';
   restaurantName.innerText = 'la pâtisserie';
+  
+  menuButton.innerText = 'menu';
 
   content.appendChild(welcome);
   content.appendChild(restaurantName);
-  
+  content.appendChild(image);
+  content.appendChild(menuButton);
+
   return containerDiv.appendChild(content);
 }
 
